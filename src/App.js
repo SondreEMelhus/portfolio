f2431cfd30cede49af383feb25eb78a47f8f57c7
Sondre Melhus <42571126+SondreEMelhus@users.noreply.github.com>
2022-09-16 22:24:26 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+
+//External packages
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react'
+
+//Router components
+import About from './components/About'
+import Work from './components/Work'
+import Contact from './components/Contact'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ChakraProvider>
+        <div className="App">
+          <header className="App-header">
+            
+          </header>
+          <div>
+            <Routes>
+              <Route path="/" element={<About/>} />
+              <Route path="/translator" element={<Work />} />
+              <Route path="/profile" element={<Contact />} />
+            </Routes>
+          </div>
+        </div>
+      </ChakraProvider>
+    </Router>
   );
 }
 
