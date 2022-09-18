77ae2cc4bc3b85e5d172d48e6894898a2f8a789f
Sondre Melhus <42571126+SondreEMelhus@users.noreply.github.com>
2022-09-18 15:59:18 +0200

    Added workin navbar with theme toggle

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+
+import theme from './components/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
