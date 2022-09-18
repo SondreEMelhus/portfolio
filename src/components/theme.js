22e8c83c807463a1e66f2aae5e3a8883d54caa74
Sondre Melhus <42571126+SondreEMelhus@users.noreply.github.com>
2022-09-18 16:36:41 +0200

    Tweaked navbar buttons for responsive design

--- src/components/theme.js
+++ src/components/theme.js
@@ -9,7 +9,15 @@ const config = {
   useSystemColorMode: false,
 }
 
+//Breakpoint
+const breakpoints = {
+  sm: '30em',
+  md: '48em',
+  lg: '62em',
+  xl: '80em',
+}
+
 // 3. extend the theme
-const theme = extendTheme({ config })
+const theme = extendTheme({ config, breakpoints })
 
 export default theme
