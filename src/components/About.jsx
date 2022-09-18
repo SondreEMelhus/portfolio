93abf65823670bbd5e04ac07d4f3dc1fd8208709
Sondre Melhus <42571126+SondreEMelhus@users.noreply.github.com>
2022-09-18 20:27:46 +0200

    Added more functionality to the navbar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, useColorMode } from "@chakra-ui/react";
 
 import Navbar from "./Navbar";
 
+ 
 export default function About () {
+
+    const { colorMode } = useColorMode();
 
     return (
         <div>
             <Navbar />
-           <Box w='80%' bg='#F6511D' color='black' border='2px' borderColor='#F6511D' borderRadius='5px' fontSize='md' pt={5} pb={5} ml='auto' mr ='auto' mt={20}>About</Box>
+           <Box w='70%' bg={colorMode === 'light' ? '#70C3FF': '#F6511D'} color='black' border='2px' borderColor={colorMode === 'light' ? '#70C3FF': '#F6511D'} borderRadius='5px' fontSize='md' pt={5} pb={5} ml='auto' mr ='auto' mt='5%'>
+               <Heading>About</Heading>
+            </Box>
+            <Box mt={20}>
+                <p>My name is Sondre Melhus, and Im a {new Date().getFullYear() - 1997} year old full-stack developer from Bergen, Norway.</p>
+            </Box>
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, useColorMode } from "@chakra-ui/react";
 
 import Navbar from "./Navbar";
 
 export default function Home () {
 
+    const { colorMode } = useColorMode();
+
     return (
         <div>
             <Navbar />
-            <Box w='80%' bg='#F6511D' color='black' border='2px' borderColor='#F6511D' borderRadius='5px' fontSize='md' pt={5} pb={5} ml='auto' mr ='auto' mt='10%'>Hello! My name is Sondre Melhus, and Im a full-stack</Box>
+            <Box w='70%' bg={colorMode === 'light' ? '#70C3FF': '#F6511D'} color='black' border='2px' borderColor={colorMode === 'light' ? '#70C3FF': '#F6511D'} borderRadius='5px' fontSize='md' pt={5} pb={5} ml='auto' mr ='auto' mt='5%'>
+                <Heading mb={5}>Welcome!</Heading> <p>My name is Sondre Melhus, and Im a full-stack developer from Bergen, Norway 🇳🇴</p>
+                </Box>
         </div>
     )
 }
